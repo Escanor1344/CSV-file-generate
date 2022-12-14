e649d1925fa9a125d30cd1f858e13a9b8965fec3
Ruslan <ruslan84598459@gmail.com>
2022-12-14 14:46:52 +0200

    settings fixes

--- CSVfile/settings.py
+++ CSVfile/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8$!&ks$*s1m)16y@o-1d4t6f*+6jxtf88hs#nizicgf__f61_#'
+SECRET_KEY = '###'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
